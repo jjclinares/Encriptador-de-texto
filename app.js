3408cc0e0936cdbf3ce974573cdc5ccf65a91ed8
jjclinares <jamer_linares@hotmail.com>
2024-08-15 16:00:14 -0500

    Responsive/Theme-dark-and-light/ended

--- app.js
+++ app.js
@@ -1,138 +1,171 @@
 /*seleccionar elementos HTML por su etiqueta, clase o ID.*/
 
-    const textArea = document.querySelector('#areaTexto');
-    const buttonEncrypt = document.querySelector('.encrypt');
-    const buttonDecrypt = document.querySelector('.decrypt');
-    const resultArea = document.querySelector('.result');
-    const copyButton = document.querySelector('.copy');
+// Selección de Elementos HTML
+const textArea = document.querySelector('#areaTexto');
+const buttonEncrypt = document.querySelector('.encrypt');
+const buttonDecrypt = document.querySelector('.decrypt');
+const resultArea = document.querySelector('#result');
+const copyButton = document.querySelector('.copy');
+const selectTema = document.getElementById('header__tema');
+const link = document.getElementById('theme-link');
 
 
+// Obtiene el Texto del TextArea
+function obtenerTexto() {
+    let texto = textArea.value.trim();
+    if (!texto) {
+        alert('Por favor, ingresa un texto.');
+        return null;
+    }
+    return texto;
+}
 
+// Muestra el Resultado en el TextArea de Resultado
+function mostrarResultado(texto) {
+    resultArea.textContent = texto;
+}
 
-    function obtenerTexto() {
-        let texto = textArea.value.trim();
-        if (!texto) {
-            alert('Por favor, ingresa un texto.');
-            return null;
+// Encriptar texto iterando por carácter
+function encriptar(texto) {
+    if (typeof texto !== 'string' || !/^[a-z\s]+$/.test(texto)) {
+        //  :^[a-z]+$: Coincide con cadenas que consisten únicamente en letras minúsculas sin acentos.
+        //  \s:espacios.
+        //  /.test(texto): Comprueba si texto cumple con esta condición.
+        //  !: Invierte el resultado. si texto contiene algo que no sea letras minúsculas sin acentos,
+        //  el resultado será true. 
+        alert('Por favor, digite solo letras minúsculas y sin acentos');
+        return;
+    } else {
+        let textoEncriptado = '';
+        for (let char of texto) {
+            switch (char) {
+                case 'a':
+                    textoEncriptado += 'ai';
+                    break;
+                case 'e':
+                    textoEncriptado += 'enter';
+                    break;
+                case 'i':
+                    textoEncriptado += 'imes';
+                    break;
+                case 'o':
+                    textoEncriptado += 'ober';
+                    break;
+                case 'u':
+                    textoEncriptado += 'ufat';
+                    break;
+                default:
+                    textoEncriptado += char;
+                    break;
+            }
         }
-        return texto;
+    mostrarResultado(textoEncriptado);
     }
+}
 
-
-    function mostrarResultado(texto) {
-        resultArea.textContent = texto;
-    }
-
-    // Encriptar texto iterando por carácter
-    function encriptar(texto) {
-        if (typeof texto !== 'string' || !/^[a-z\s]+$/.test(texto)) {
-            //  :^[a-z]+$: Coincide con cadenas que consisten únicamente en letras minúsculas sin acentos.
-            //  \s:espacios.
-            //  /.test(texto): Comprueba si texto cumple con esta condición.
-            //  !: Invierte el resultado. si texto contiene algo que no sea letras minúsculas sin acentos,
-            //  el resultado será true. 
-            alert('Por favor, digite solo letras minúsculas y sin acentos');
-            return;
+// Desencripta texto iterando por carácter
+function desencriptar(texto) {
+    let textoDesencriptado = '';
+    for (let i = 0; i < texto.length; i++) {
+        if (texto.startsWith('ai', i)) {
+            textoDesencriptado += 'a';
+            i += 1; // Saltar los caracteres ya procesados
+        } else if (texto.startsWith('enter', i)) {
+            textoDesencriptado += 'e';
+            i += 4;
+        } else if (texto.startsWith('imes', i)) {
+            textoDesencriptado += 'i';
+            i += 3;
+        } else if (texto.startsWith('ober', i)) {
+            textoDesencriptado += 'o';
+            i += 3;
+        } else if (texto.startsWith('ufat', i)) {
+            textoDesencriptado += 'u';
+            i += 3;
         } else {
-            let textoEncriptado = '';
-            for (let char of texto) {
-                switch (char) {
-                    case 'a':
-                        textoEncriptado += 'ai';
-                        break;
-                    case 'e':
-                        textoEncriptado += 'enter';
-                        break;
-                    case 'i':
-                        textoEncriptado += 'imes';
-                        break;
-                    case 'o':
-                        textoEncriptado += 'ober';
-                        break;
-                    case 'u':
-                        textoEncriptado += 'ufat';
-                        break;
-                    default:
-                        textoEncriptado += char;
-                        break;
-                }
-            }
-        mostrarResultado(textoEncriptado);
+            textoDesencriptado += texto[i];
         }
     }
+    mostrarResultado(textoDesencriptado);
+}
 
-
-
-    // Desencripta el texto iterando por cada carácter
-    function desencriptar(texto) {
-        let textoDesencriptado = '';
-        for (let i = 0; i < texto.length; i++) {
-            if (texto.startsWith('ai', i)) {
-                textoDesencriptado += 'a';
-                i += 1; // Saltar los caracteres ya procesados
-            } else if (texto.startsWith('enter', i)) {
-                textoDesencriptado += 'e';
-                i += 4;
-            } else if (texto.startsWith('imes', i)) {
-                textoDesencriptado += 'i';
-                i += 3;
-            } else if (texto.startsWith('ober', i)) {
-                textoDesencriptado += 'o';
-                i += 3;
-            } else if (texto.startsWith('ufat', i)) {
-                textoDesencriptado += 'u';
-                i += 3;
-            } else {
-                textoDesencriptado += texto[i];
-            }
-        }
-
-        mostrarResultado(textoDesencriptado);
+// Eventos de los Botones de Encriptar y Desencriptar
+buttonEncrypt.addEventListener('click', () => {
+    const texto = obtenerTexto();
+    if (texto) {
+        encriptar(texto);
     }
+});
 
-
-    // Eventos de los botones
-
-    buttonEncrypt.addEventListener('click', () => {
-        const texto = obtenerTexto();
-        if (texto) {
-            encriptar(texto);
-        }
-    });
-
-    buttonDecrypt.addEventListener('click', () => {
-        const texto = obtenerTexto();
-        if (texto) {
-            desencriptar(texto);
-        }
-    });
-
-    // Copiar el texto al portapapeles
-    function copiarTexto() {
-        navigator.clipboard.writeText(resultArea.textContent)
-            .then(() => console.log('Texto copiado al portapapeles'))
-            .catch(err => console.error('Error al copiar:', err));
+buttonDecrypt.addEventListener('click', () => {
+    const texto = obtenerTexto();
+    if (texto) {
+        desencriptar(texto);
     }
+});
 
+// Copiar el texto al portapapeles
+function copiarTexto() {
+    navigator.clipboard.writeText(resultArea.textContent)
+        .then(() => mostrarAlerta('Texto copiado al portapapeles'))
+        .catch(err => console.log('Error al copiar:', err));
+}
 copyButton.addEventListener('click', copiarTexto);
 
 
-
-    window.addEventListener('load', function() {
-        const editor = new window.LanguageToolEmbed({
-        container: document.getElementById('areaTexto'),
-        apiUrl: 'https://api.languagetoolplus.com'
+// Usar el servidor LanguageTool
+window.addEventListener('load', function() {
+    const editor = new window.LanguageToolEmbed({
+    container: document.getElementById('areaTexto'),
+    apiUrl: 'https://api.languagetoolplus.com'
     });
-    editor.on('update', function(status) {
-        const toolbar = document.querySelector('.lt-toolbar');
-        if (status.errorCount > 0) {
-            toolbar.classList.remove('status-icon--has-no-errors');
-            toolbar.classList.add('status-icon--has-errors');
-            toolbar.setAttribute('title', `${status.errorCount} errores encontrados`);
-        } else {
-            toolbar.classList.remove('status-icon--has-errors');
-            toolbar.classList.add('status-icon--has-no-errors');
-            toolbar.setAttribute('title', 'No se encontraron errores');
-        }
+editor.on('update', function(status) {
+    const toolbar = document.querySelector('.lt-toolbar');
+    if (status.errorCount > 0) {
+        toolbar.classList.remove('status-icon--has-no-errors');
+        toolbar.classList.add('status-icon--has-errors');
+        toolbar.setAttribute('title', `${status.errorCount} errores encontrados`);
+    } else {
+        toolbar.classList.remove('status-icon--has-errors');
+        toolbar.classList.add('status-icon--has-no-errors');
+        toolbar.setAttribute('title', 'No se encontraron errores');
+    }
     });
 });
+
+// Función para Cambiar de Tema (Claro/Oscuro)
+function cambiarTema() {
+    const temaSeleccionado = selectTema.value;
+    
+    if (temaSeleccionado === 'dark') {
+        link.href = './styles/darkstyle.css'; 
+        document.getElementById('videoBackground').querySelector('source').src = './assets/darkThemeVideo.mp4';
+        localStorage.setItem('theme', 'dark'); // Guarda la preferencia en localStorage
+    } else {
+        link.href = './styles/style.css'; 
+        document.getElementById('videoBackground').querySelector('source').src = './assets/lightThemeVideo.mp4'; 
+        localStorage.setItem('theme', 'light'); // Guarda la preferencia en localStorage
+    }
+    
+    document.getElementById('videoBackground').load(); // Recarga el video
+}
+
+// Inicializa el tema basado en la preferencia guardada o el tema predeterminado (light)
+function inicializarTema() {
+    const temaGuardado = localStorage.getItem('theme') || 'dark';
+
+    if (temaGuardado === 'dark') {
+        link.href = './styles/darkstyle.css';
+        document.getElementById('videoBackground').querySelector('source').src = './assets/darkThemeVideo.mp4';
+        selectTema.value = 'dark';
+    } else {
+        link.href = './styles/style.css';
+        document.getElementById('videoBackground').querySelector('source').src = './assets/lightThemeVideo.mp4';
+        selectTema.value = 'light'; 
+    }
+
+    document.getElementById('videoBackground').load(); // Recarga el video
+}
+
+// Llama a la función para inicializar el tema cuando la página se carga
+window.addEventListener('load', inicializarTema);
---
+++ readme/README.md
@@ -0,0 +1,47 @@
+# <h1 align="center"> Encriptador-de-texto ORACLE alura</h1>
+-git status: ended
+
+[This site was built using [GitHub Pages and Visual Studio Code]
+
+![Logo Oracle ONE alura ](./aluraoracle.png)!
+
+Ante la necesidad de transmitir información escrita de manera privada nace el mensaje encriptado.Buscando suplir esta necesidad nace esta pagina web, la cual toma un mensaje **INICIAL** del emisor el cual cambiara sus vocales con el fin de proteger la información, hasta que llegue a las manos del receptor quien usando la pagina web Desencriptara el mensaje **FINAL*.  
+
+
+Con las bases adquiridas en el programa **ONE** de la empresa **Oracle** usando lenguajes de demarcado __HTML__ dandole un diseño estimado con __CSS__ siguiendo el diseño de *FIGMA* y agregando las fucnionalidades con **JavaScript**. se presentan las condiciones bajo la cuales debe desarrollarse la solución:
+
+
+## Condiciones
+
+Vamos a trabajar en una aplicación que encripta textos, así podrás intercambiar mensajes secretos con otras personas que sepan el secreto de la encriptación utilizada.
+
+Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"
+
+### Requisitos:
+
+    Debe funcionar solo con letras minúsculas
+    No deben ser utilizados letras con acentos ni caracteres especiales
+    Debe ser posible convertir una palabra para la versión encriptada también devolver una palabra encriptada para su versión original.
+
+Por ejemplo:
+"gato" => "gaitober"
+gaitober" => "gato"
+
+    La página debe tener campos para
+    inserción del texto que será encriptado o desencriptado, y el usuario debe poder escoger entre las dos opciones.
+    El resultado debe ser mostrado en la pantalla.
+
+#### Extras:
+
+    Un botón que copie el texto encriptado/desencriptado para la sección de transferencia, o sea que tenga la misma funcionalidad del ctrl+C o de la opción "copiar" del menú de las aplicaciones.
+
+#### Resultado
+
+![Imagen Pagina web del Encriptador ](./Evidencia-pagina-web.png)!
+
